7309db5b5a8d19ce4df3e41e8f3431a6f880cd54
A-Kelly05 <aidan.kelly16020@est.uca.edu.ni>
2023-04-28 07:45:57 -0600

    Reposicion de prueba

---
+++ principal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int opcion;
+    float monto;
+    string operadora;
+    bool seguir = true;
+
+    while (seguir)
+    {
+        cout << "MENU" << endl;
+        cout << "1, Registrar ventas de recarga" << endl;
+    }
+    
+    return 0;
+}
