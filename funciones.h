0711adf2a2090d54dd25a66a90a86024cc3f9d37
A-Kelly05 <aidan.kelly16020@est.uca.edu.ni>
2023-04-28 08:09:26 -0600

    actualizacion

---
+++ funciones.h
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+
+map<string, float> ventasPorOperadora;
+float totalVenta = 0;
+
+void registarventa(float)
--- principal.cpp
+++ principal.cpp
@@ -10,11 +10,33 @@ int main(int argc, char const *argv[])
     string operadora;
     bool seguir = true;
 
-    while (seguir)
-    {
+    while (seguir){
         cout << "MENU" << endl;
         cout << "1, Registrar ventas de recarga" << endl;
+        cout << "2, Mostrar total de ventas de recarga" << endl;
+        cout << "3, Mostrar venta de recarga por operadora" << endl;
+        cout << "4, salir" << endl;
+        cout << "Ingresar opcion:  ";
+        cin >> opcion;
+
+        switch (opcion){
+        case 1:
+            cout << "Ingrese el monto de recarga: ";
+            cin >> monto;
+            cout << "Ingrese la operadora(Tigo o Claro)";
+            cin >> operadora;
+            
+
+
+            break;
+        
+        default:
+            break;
+        }
+
+
     }
     
     return 0;
 }
+
